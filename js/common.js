4ab52c98e1612d3134bef69700b7f2ce0e7d62f8
Kartel Maksym <kartmax1804@gmail.com>
2023-11-19 21:18:30 +0100

    3d parallax effect

---
+++ js/common.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	document.addEventListener('mousemove', e => {
+      Object.assign(document.documentElement, {
+         style: `
+            --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+            --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
+         `
+      })
+   });
+
+});
